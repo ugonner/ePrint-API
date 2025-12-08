a6e8ae5be542a90712ef50e959f34ad87ad44dfb
ugonner <umeokwonnabonaventure@gmail.com>
2025-12-08 15:06:29 +0100

    integrates with db

--- src/aid-service/datasets/seed.ts
+++ src/aid-service/datasets/seed.ts
@@ -1,37 +1,44 @@
 export const seedServices = [
   {
-    name: "Printing",
-    description: "Quality black-and-white or colored printing for documents, forms, and personal files.",
-    serviceRate: 100
+    name: 'Printing',
+    description:
+      'Quality black-and-white or colored printing for documents, forms, and personal files.',
+    serviceRate: 100,
   },
   {
-    name: "Document Typing",
-    description: "Professional typing of letters, assignments, reports, forms, and other documents.",
-    serviceRate: 300
+    name: 'Typing',
+    description:
+      'Professional typing of letters, assignments, reports, forms, and other documents.',
+    serviceRate: 300,
   },
   {
-    name: "Binding",
-    description: "Secure document binding for projects, reports, and presentations.",
-    serviceRate: 250
+    name: 'Binding',
+    description:
+      'Secure document binding for projects, reports, and presentations.',
+    serviceRate: 250,
+  },
+
+  {
+    name: 'Scanning',
+    description:
+      'High-resolution scanning to convert documents into digital formats.',
+    serviceRate: 150,
   },
   {
-    name: "Lamination",
-    description: "Durable lamination to protect certificates, IDs, photos, and documents.",
-    serviceRate: 200
+    name: 'Lamination',
+    description:
+      'Durable lamination to protect certificates, IDs, photos, and documents.',
+    serviceRate: 200,
   },
   {
-    name: "Graphic Designs",
-    description: "Custom designs for cards, flyers, invitations, certificates, and simple branding.",
-    serviceRate: 400
+    name: 'Photocopying',
+    description: 'Clean and fast photocopying for single or bulk documents.',
+    serviceRate: 50,
   },
   {
-    name: "Photocopying",
-    description: "Clean and fast photocopying for single or bulk documents.",
-    serviceRate: 50
+    name: 'Graphic Designs',
+    description:
+      'Custom designs for cards, flyers, invitations, certificates, and simple branding.',
+    serviceRate: 400,
   },
-  {
-    name: "Scanning",
-    description: "High-resolution scanning to convert documents into digital formats.",
-    serviceRate: 150
-  }
 ];
